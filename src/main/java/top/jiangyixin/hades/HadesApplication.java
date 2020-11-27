468fd1767b1fd90abd22de64c779b7273ab573a7
jiangyixin1209 <17602173915@163.com>
2020-11-27 10:11:31 +0800

    init

--- src/main/java/top/jiangyixin/hades/HadesApplication.java
+++ src/main/java/top/jiangyixin/hades/HadesApplication.java
@@ -1,4 +1,15 @@
 package top.jiangyixin.hades;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiangyixin
+ */
+@SpringBootApplication
 public class HadesApplication {
+
+  public static void main(String[] args) {
+	  SpringApplication.run(HadesApplication.class, args);
+  }
 }
