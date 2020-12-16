c8c4f31e543502dd06735f4a7e1f15641c2d5337
jiangyixin1209 <17602173915@163.com>
2020-12-16 17:43:24 +0800

    增加velocity渲染

---
+++ src/main/java/top/jiangyixin/hades/common/TemplateConstant.java
@@ -0,0 +1,16 @@
+package top.jiangyixin.hades.common;
+
+/**
+ * 模板常量名
+ * @version 1.0
+ * @author jiangyixin
+ * @date 2020/12/16 下午5:36
+ */
+public class TemplateConstant {
+	public static final String ENTITY_TEMPLATE = "Entity.java.vm";
+	public static final String DAO_TEMPLATE = "Dao.java.vm";
+	public static final String DAO_XML_TEMPLATE = "Dao.xml.vm";
+	public static final String SERVICE_TEMPLATE = "Service.java.vm";
+	public static final String SERVICE_IMPL_TEMPLATE = "ServiceImpl.java.vm";
+	public static final String CONTROLLER_TEMPLATE = "Controller.java.vm";
+}
--- src/main/java/top/jiangyixin/hades/service/CodeGenService.java
+++ src/main/java/top/jiangyixin/hades/service/CodeGenService.java
@@ -5,13 +5,20 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.WordUtils;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import top.jiangyixin.hades.common.TemplateConstant;
 import top.jiangyixin.hades.entity.Column;
 import top.jiangyixin.hades.entity.Table;
 import top.jiangyixin.hades.exception.HadesException;
+import top.jiangyixin.hades.util.DateUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.*;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -39,52 +46,34 @@ public class CodeGenService {
 		return templates;
 	}
 	
+	/**
+	 * 生成代码
+	 * @param tableInfo         表信息
+	 * @param columnsInfo       列信息
+	 * @param zipOutputStream   压缩输出流
+	 */
 	public static void generateCode(Map<String, String> tableInfo,
 	                                List<Map<String, String>> columnsInfo,
 	                                ZipOutputStream zipOutputStream) {
 		Configuration configuration = getConfiguration();
-		boolean hasBigDecimal = false;
-		boolean hasList = false;
+		Table table = loadSingleTable(configuration, tableInfo, columnsInfo);
+		initVelocity();
+		VelocityContext context = loadTemplateContext(configuration, table);
 		
-		Table table = new Table();
-		table.setTableName(tableInfo.get("tableName"));
-		table.setComment(tableInfo.get("tableComment"));
-		String[] tablePrefixArray = configuration.getString("tablePrefix").split(",");
-		String clazzName = tableToJava(table.getTableName(), tablePrefixArray);
-		table.setUpClassName(clazzName);
-		table.setLowClassName(StringUtils.uncapitalize(clazzName));
-		List<Column> columns = new ArrayList<>();
-		for (Map<String, String> columnInfo: columnsInfo) {
-			Column column = new Column();
-			column.setColumnName(columnInfo.get("columnName"));
-			column.setDataType(columnInfo.get("dataType"));
-			column.setComment(columnInfo.get("columnComment"));
-			column.setExtra(columnInfo.get("extra"));
-			// 列名转Java属性名
-			column.setUpAttrName(columnToJava(column.getColumnName()));
-			column.setLowAttrName(StringUtils.uncapitalize(column.getUpAttrName()));
-			// 列数据类型转Java类型
-			String attrType = configuration.getString(column.getDataType().toLowerCase(),
-					columnToJava(column.getDataType()));
-			column.setAttrType(attrType);
-			
-			if (!hasBigDecimal && "BigDecimal".equals(attrType)) {
-				hasBigDecimal = true;
+		for (String template : getTemplates()) {
+			StringWriter sw = new StringWriter();
+			Template tpl = Velocity.getTemplate(template, "UTF_8");
+			tpl.merge(context, sw);
+			try{
+				String filename = getFilename(
+						template, table.getUpClassName(),
+						configuration.getString("package"),
+						configuration.getString("moduleName"));
+				zipOutputStream.putNextEntry(new ZipEntry(filename));
+			} catch (IOException e) {
+				throw new HadesException("渲染模板失败，表: " + table.getTableName(), e);
 			}
-			
-			// 设置主键
-			if ("PRI".equalsIgnoreCase(columnInfo.get("columnKey")) && table.getPk() == null) {
-				table.setPk(column);
-			}
-			columns.add(column);
 		}
-		table.setColumns(columns);
-		
-		// 如果未设置主键则默认为第一个字段
-		if (table.getPk() == null) {
-			table.setPk(table.getColumns().get(0));
-		}
-		
 	}
 	
 	public static Configuration getConfiguration() {
@@ -132,9 +121,113 @@ public class CodeGenService {
 		return columnToJava(tableName);
 	}
 	
-	public static v
-
-	  public static void main(String[] args) {
-	     System.out.println(columnToJava("sys_user"));
-	  }
+	/**
+	 * 初试化velocity，设置velocity资源加载器
+	 */
+	public static void initVelocity() {
+		Properties properties = new Properties();
+		properties.put("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        Velocity.init(properties);
+	}
+	
+	public static String getFilename(String template, String className, String packageName, String moduleName) {
+		String packagePath = "main" + File.separator + "java" + File.separator;
+		if (StringUtils.isNotBlank(packageName)) {
+			packagePath += packageName.replace(".", File.separator) + File.separator + moduleName + File.separator;
+		}
+		if (template.contains(TemplateConstant.ENTITY_TEMPLATE)) {
+			return packagePath + "entity" + File.separator + className + "Entity.java";
+		}
+		if (template.contains(TemplateConstant.DAO_TEMPLATE)) {
+			return packagePath + "dao" + File.separator + className + "DAO.java";
+		}
+		if (template.contains(TemplateConstant.SERVICE_TEMPLATE)) {
+			return packagePath + "service" + File.separator + className + "Service.java";
+		}
+		if (template.contains(TemplateConstant.SERVICE_IMPL_TEMPLATE)) {
+			return packagePath + "service" + File.separator + "impl" + File.separator + className + "ServiceImpl.java";
+		}
+		if (template.contains(TemplateConstant.CONTROLLER_TEMPLATE)) {
+			return packagePath + "controller" + File.separator + className + "Controller.java";
+		}
+		if (template.contains(TemplateConstant.DAO_XML_TEMPLATE)) {
+			return "main" + File.separator + "resources" + File.separator + "mapper" +
+					File.separator + moduleName + File.separator + className + "DAO.xml";
+		}
+		return null;
+	}
+	
+	/**
+	 * 加载单表对象
+	 * @param configuration     配置
+	 * @param tableInfo         表信息
+	 * @param columnsInfo       列信息
+	 * @return                  Table
+	 */
+	public static Table loadSingleTable(Configuration configuration,
+	                                    Map<String, String> tableInfo,
+	                                    List<Map<String, String>> columnsInfo) {
+		boolean hasBigDecimal = false;
+		Table table = new Table();
+		table.setTableName(tableInfo.get("tableName"));
+		table.setComment(tableInfo.get("tableComment"));
+		String[] tablePrefixArray = configuration.getString("tablePrefix").split(",");
+		String className = tableToJava(table.getTableName(), tablePrefixArray);
+		table.setUpClassName(className);
+		table.setLowClassName(StringUtils.uncapitalize(className));
+		List<Column> columns = new ArrayList<>();
+		for (Map<String, String> columnInfo: columnsInfo) {
+			Column column = new Column();
+			column.setColumnName(columnInfo.get("columnName"));
+			column.setDataType(columnInfo.get("dataType"));
+			column.setComment(columnInfo.get("columnComment"));
+			column.setExtra(columnInfo.get("extra"));
+			// 列名转Java属性名
+			column.setUpAttrName(columnToJava(column.getColumnName()));
+			column.setLowAttrName(StringUtils.uncapitalize(column.getUpAttrName()));
+			// 列数据类型转Java类型
+			String attrType = configuration.getString(column.getDataType().toLowerCase(),
+					columnToJava(column.getDataType()));
+			column.setAttrType(attrType);
+			
+			if (!hasBigDecimal && "BigDecimal".equals(attrType)) {
+				hasBigDecimal = true;
+			}
+			
+			// 设置主键
+			if ("PRI".equalsIgnoreCase(columnInfo.get("columnKey")) && table.getPk() == null) {
+				table.setPk(column);
+			}
+			columns.add(column);
+		}
+		table.setColumns(columns);
+		
+		// 如果未设置主键则默认为第一个字段
+		if (table.getPk() == null) {
+			table.setPk(table.getColumns().get(0));
+		}
+		return table;
+	}
+	
+	/**
+	 * 封装模板上下文
+	 * @param configuration     配置对象
+	 * @param table             表对象
+	 * @return                  VelocityContext
+	 */
+	public static VelocityContext loadTemplateContext(Configuration configuration, Table table) {
+		Map<String, Object> templateData = new HashMap<>();
+		templateData.put("tableName", table.getTableName());
+		templateData.put("comment", table.getComment());
+		templateData.put("pk", table.getPk());
+		templateData.put("lowClassName", table.getLowClassName());
+		templateData.put("upClassName", table.getUpClassName());
+		templateData.put("datetime", DateUtils.format(new Date(), DateUtils.DATETIME_PATTERN));
+		templateData.put("packageName", configuration.getString("package"));
+		templateData.put("moduleName", configuration.getString("module"));
+		templateData.put("author", configuration.getString("author"));
+		templateData.put("email", configuration.getString("email"));
+		return new VelocityContext(templateData);
+	}
+	
 }
